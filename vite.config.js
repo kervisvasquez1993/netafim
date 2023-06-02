becbe0c79ccea7f63698aa0af454a6db7cc93a09
kvasquez123 <kvasquez@qantamedia.com>
2023-06-02 10:08:16 -0400

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,63 @@
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+// import { VitePWA } from "vite-plugin-pwa";
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    reactRefresh(),
-    VitePWA({
-      manifest: {
-        name: 'Netafim',
-        short_name: 'Netafim',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/assets/icons/logo192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/assets/icons/logo512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-        permissions: ['camera']
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: new RegExp('^https://netafim-app-ksw6o.ondigitalocean.app/api/api/'),
-            handler: 'StaleWhileRevalidate',
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxAgeSeconds: 60 * 60 * 24 * 7, // 1 semana
-              },
-              cacheableResponse: {
-                statuses: [200],
-              },
-            },
-          },
-        ],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-});
+  plugins: [react()],
+})
+vite.config.js
+// import { defineConfig } from 'vite';
+// import { VitePWA } from 'vite-plugin-pwa';
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+// import { resolve } from 'path';
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     reactRefresh(),
+//     VitePWA({
+//       manifest: {
+//         name: 'Netafim',
+//         short_name: 'Netafim',
+//         theme_color: '#ffffff',
+//         icons: [
+//           {
+//             src: '/assets/icons/icon-192x192.png',
+//             sizes: '192x192',
+//             type: 'image/png',
+//           },
+//           {
+//             src: '/assets/icons/logo512x512.png',
+//             sizes: '512x512',
+//             type: 'image/png',
+//           },
+//         ],
+//         permissions: ['camera']
+//       },
+//       workbox: {
+//         runtimeCaching: [
+//           {
+//             urlPattern: new RegExp('^https://netafim-app-ksw6o.ondigitalocean.app/api/api/'),
+//             handler: 'StaleWhileRevalidate',
+//             options: {
+//               cacheName: 'api-cache',
+//               expiration: {
+//                 maxAgeSeconds: 60 * 60 * 24 * 7, // 1 semana
+//               },
+//               cacheableResponse: {
+//                 statuses: [200],
+//               },
+//             },
+//           },
+//         ],
+//       },
+//     }),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': resolve(__dirname, 'src'),
+//     },
+//   },
+// });
