25eab5b93f6874c6e4bbd4596a6e8cf67c5f0386
kvasquez123 <kvasquez@qantamedia.com>
2023-06-08 14:43:36 -0400

    update field pencil in edit password

--- src/Pages/Me.jsx
+++ src/Pages/Me.jsx
@@ -72,15 +72,15 @@ const Me = () => {
                         readOnly
                     />
                 </div>
-                <div className="mb-4 form-container">
+                <div className="mb-4 form-container input-container">
                     <label
                         htmlFor="password"
-                        className="label-position label-style"
+                        className="label-position label-style "
                     >
                         Password
                     </label>
                     <input
-                        className="input-style"
+                        className="input-style input-field"
                         placeholder="*********"
                         type="password"
                         id="password"
@@ -88,13 +88,28 @@ const Me = () => {
                         onChange={(e) => setPassword(e.target.value)}
                         value={password} // Reemplazar con el valor correcto del objeto 'auth'
                     />
+                    <svg
+                        className="icon h-5 w-5 "
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                        />
+                    </svg>
                 </div>
                 <div className="mb-4 form-container flex justify-center">
-                <input
-                    type="submit"
-                    value="Editar clave"
-                    className="button-style mb-5"
-                />
+                    <input
+                        type="submit"
+                        value="Editar clave"
+                        className="button-style mb-5"
+                    />
                 </div>
             </form>
         </>
