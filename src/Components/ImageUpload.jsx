3ac01f6b93c22d6420a86a1f9976a770ce262afd
kvasquez123 <kvasquez@qantamedia.com>
2023-06-12 08:43:36 -0400

    feat: update style in upload imagen

--- src/Components/ImageUpload.jsx
+++ src/Components/ImageUpload.jsx
@@ -1,104 +1,121 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import imagenUpload from "../assets/sin-img.png";
 import { useParams } from "react-router-dom";
 import useCard from "../Hooks/useCard";
 import Swal from "sweetalert2";
 
 function ImageUploader() {
-    const fileInputRef = useRef(null);
-    const [imageSrc, setImageSrc] = useState(imagenUpload);
-    const [isLoading, setIsLoading] = useState(false);
-    const { submitNewTaerjeta } = useCard();
-    const params = useParams();
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { submitNewTaerjeta } = useCard();
+  const params = useParams();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const selectedFile = fileInputRef.current.files[0];
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        if (!selectedFile) {
-            Swal.fire({
-                title: "Error!",
-                text: "No se ha seleccionado ningún archivo",
-                icon: "error",
-                confirmButtonText: "Aceptar",
-            });
-            return;
-        }
+    if (!selectedFile) {
+      Swal.fire({
+        title: "Error!",
+        text: "No se ha seleccionado ninguna imagen",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+      return;
+    }
 
-        setIsLoading(true);
+    setIsLoading(true);
 
-        try {
-            const formData = new FormData();
-            formData.append("src_img", selectedFile);
-            if (params.id) {
-                console.log("params.id", params.id);
-                await submitNewTaerjeta(formData, params.id);
-            } else {
-                console.log("params.id sin esto");
-                await submitNewTaerjeta(formData);
-            }
+    try {
+      const formData = new FormData();
+      formData.append("src_img", selectedFile);
+      if (params.id) {
+        console.log("params.id", params.id);
+        await submitNewTaerjeta(formData, params.id);
+      } else {
+        console.log("params.id sin esto");
+        await submitNewTaerjeta(formData);
+      }
 
-            setIsLoading(false);
-        } catch (error) {
-            setIsLoading(false);
-            Swal.fire({
-                title: "Error!",
-                text: "Error al enviar la imagen",
-                icon: "error",
-                confirmButtonText: "Aceptar",
-            });
-        }
-    };
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      Swal.fire({
+        title: "Error!",
+        text: "Error al enviar la imagen",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    }
+  };
 
-    const handleImageClick = () => {
-        fileInputRef.current.click();
-    };
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
-    const handleFileChange = () => {
-        const selectedFile = fileInputRef.current.files[0];
-        setImageSrc(URL.createObjectURL(selectedFile));
-    };
+  const handleCancelImage = () => {
+    setSelectedFile(null);
+    document.getElementById("file-input").value = "";
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className="bg-white rounded-lg border border-gray-400 p-6 mt-10 mb-10">
-                <label htmlFor="file-input" className="block mb-4">
-                    <div className="border-dashed border-2 border-gray-400 rounded-lg p-4">
-                        {isLoading ? (
-                            <p>Cargando archivo...</p>
-                        ) : (
-                            <img
-                                src={imageSrc}
-                                alt="Selecciona una imagen"
-                                className="block mx-auto cursor-pointer object-contain w-full h-full"
-                                onClick={handleImageClick}
-                            />
-                        )}
-                    </div>
-                </label>
-                <input
-                    ref={fileInputRef}
-                    id="file-input"
-                    type="file"
-                    className="hidden"
-                    name="src_img"
-                    onChange={handleFileChange}
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="bg-white rounded-lg shadow-md pr-10 pl-10 mt-10 mb-10 min-height-200px">
+      {selectedFile? (
+                <img
+                  src={URL.createObjectURL(selectedFile)}
+                  alt="Imagen seleccionada"
+                  className="block mx-auto cursor-pointer object-contain w-full h-full min-height-200px"
                 />
-            </div>
-
-            <div className={`flex justify-center pt-10 pb-5`}>
-                <button
-                    className={
-                        "px-4 py-2 rounded-md font-medium border bg-blue-500 text-white hover:bg-blue-600"
-                    }
-                    style={{ minWidth: "200px", height: "50px" }}
-                    type="submit"
-                >
-                    Subir Archivo
-                </button>
-            </div>
-        </form>
-    );
+              ) : ( <img
+                src={imagenUpload}
+                alt="Imagen seleccionada"
+                className="block mx-auto cursor-pointer object-contain w-full h-full min-height-200px"
+              />)}       
+        <input
+          id="file-input"
+          type="file"
+          className="hidden"
+          name="src_img"
+          onChange={handleFileChange}
+        />
+      </div>
+      <div className="">
+  {isLoading ? (
+    <p>Cargando archivo...</p>
+  ) : (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <label
+          htmlFor="file-input"
+          className="button-style-2 text-center  m-5"
+        >
+          {selectedFile ? "Cambiar imagen" : "Seleccionar imagen"}
+        </label>
+        {selectedFile && (
+          <button
+            type="button"
+            className="button-style-red text-center "
+            onClick={handleCancelImage}
+          >
+            Cancelar
+          </button>
+        )}
+      </div>
+    </>
+  )}
+</div>
+      <div className="flex justify-center pt-10 pb-5">
+        <button
+          className="button-style"
+          style={{ minWidth: "200px", height: "50px" }}
+          type="submit"
+          disabled={isLoading || !selectedFile}
+        >
+          {isLoading ? "Cargando..." : "Subir Archivo"}
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default ImageUploader;
