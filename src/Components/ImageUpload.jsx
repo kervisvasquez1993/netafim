790ebaef453933cb66650555ea1fb85ca033056e
kvasquez123 <kvasquez@qantamedia.com>
2023-06-14 08:41:15 -0400

    camara en su posicion

--- src/Components/ImageUpload.jsx
+++ src/Components/ImageUpload.jsx
@@ -1,52 +1,28 @@
 import React, { useState, useRef } from "react";
-import imagenUpload from "../assets/sin-img.png";
-import { useParams } from "react-router-dom";
-import useCard from "../Hooks/useCard";
-import Swal from "sweetalert2";
-import Webcam from "react-webcam";
+import { Camera } from "react-camera-pro";
 
-function ImageUploader() {
+const ImageUploader = () => {
+  const cameraRef = useRef(null);
   const [selectedFile, setSelectedFile] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const { submitNewTaerjeta } = useCard();
-  const params = useParams();
-  const webcamRef = useRef(null);
+  const [cameraVisible, setCameraVisible] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (!selectedFile) {
-      Swal.fire({
-        title: "Error!",
-        text: "No se ha seleccionado ninguna imagen",
-        icon: "error",
-        confirmButtonText: "Aceptar",
-      });
+      // Mostrar mensaje de error si no se ha seleccionado ninguna imagen
       return;
     }
 
     setIsLoading(true);
 
     try {
-      const formData = new FormData();
-      formData.append("src_img", selectedFile);
-      if (params.id) {
-        console.log("params.id", params.id);
-        await submitNewTaerjeta(formData, params.id);
-      } else {
-        console.log("params.id sin esto");
-        await submitNewTaerjeta(formData);
-      }
-
+      // Procesar y enviar la imagen seleccionada
       setIsLoading(false);
     } catch (error) {
       setIsLoading(false);
-      Swal.fire({
-        title: "Error!",
-        text: "Error al enviar la imagen",
-        icon: "error",
-        confirmButtonText: "Aceptar",
-      });
+      // Mostrar mensaje de error en caso de falla en el envío de la imagen
     }
   };
 
@@ -64,43 +40,44 @@ function ImageUploader() {
   };
 
   const handleCameraButtonClick = () => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    if (imageSrc) {
-      const capturedImage = dataURLtoFile(imageSrc, "capturedImage.jpeg");
-      setSelectedFile(capturedImage);
-    }
+    setCameraVisible(!cameraVisible);
   };
 
-  const dataURLtoFile = (dataURL, filename) => {
-    const arr = dataURL.split(",");
-    const mime = arr[0].match(/:(.*?);/)[1];
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
+  const handleTakePhoto = () => {
+    const photo = cameraRef.current.takePhoto();
+
+    if (photo) {
+      const objectUrl = URL.createObjectURL(photo);
+      setSelectedFile(objectUrl);
     }
-    return new File([u8arr], filename, { type: mime });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="bg-white rounded-lg shadow-md pr-10 pl-10 mt-10 mb-10 min-height-200px">
+      <div className="bg-white rounded-lg shadow-md pr-10 pl-10 mt-10 mb-10 min-height-200px relative">
         {selectedFile ? (
           <img
-            src={URL.createObjectURL(selectedFile)}
+            src={selectedFile}
             alt="Imagen seleccionada"
             className="block mx-auto cursor-pointer object-contain w-full h-full min-height-200px"
             onClick={handleImageUploadClick}
           />
         ) : (
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            className="block mx-auto cursor-pointer object-contain w-full h-full min-height-200px"
-            onClick={handleImageUploadClick}
-          />
+          <>
+            {cameraVisible && (
+              <div className="camera-container absolute inset-0 flex items-center justify-center">
+                <Camera ref={cameraRef} />
+              </div>
+            )}
+            {!cameraVisible && (
+              <button
+                className="camera-toggle-button absolute inset-0 w-full h-full bg-transparent text-center"
+                onClick={handleCameraButtonClick}
+              >
+                Mostrar cámara
+              </button>
+            )}
+          </>
         )}
         <input
           id="file-input"
@@ -124,13 +101,15 @@ function ImageUploader() {
               >
                 {selectedFile ? "Cambiar imagen" : "Seleccionar imagen"}
               </label>
-              <button
-                type="button"
-                className="button-style-2 text-center m-5"
-                onClick={handleCameraButtonClick}
-              >
-                Tomar foto
-              </button>
+              {cameraVisible && (
+                <button
+                  type="button"
+                  className="button-style-2 text-center m-5"
+                  onClick={handleTakePhoto}
+                >
+                  Tomar foto
+                </button>
+              )}
               {selectedFile && (
                 <button
                   type="button"
@@ -156,6 +135,6 @@ function ImageUploader() {
       </div>
     </form>
   );
-}
+};
 
 export default ImageUploader;
